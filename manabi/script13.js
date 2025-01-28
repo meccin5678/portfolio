a7ff736492a33f2557fb4dd83feac796f42b0753
meccin56 <meccin56@gmail.com>
2025-01-28 23:10:34 +0900

    大入れ替え

--- manabi/script13.js
+++
@@ -1,7 +0,0 @@
-const loaderContainer = document.querySelector('.loader-container');
-const section = document.querySelector('.section');
-
-window.addEventListener('click', () => {
-  loaderContainer.classList.add('hidden');
-  section.classList.add('visible');
-});
