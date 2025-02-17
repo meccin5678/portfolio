f332729dcbd115f5ea34ced7550d00365c511c8c
meccin56 <meccin56@gmail.com>
2025-02-17 17:47:43 +0900

    Remove reactA directory

--- reactA/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/portfolio/reactA/', // ベースURLを設定
-  plugins: [react()],
-});
