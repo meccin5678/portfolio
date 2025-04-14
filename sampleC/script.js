dd887a5ce5882c90d772418cd4c3534df2a78993
meccin5678 <meccin56@gmail.com>
2025-04-14 21:44:45 +0900

    sampleCを作りました

---
+++ sampleC/script.js
@@ -0,0 +1,8 @@
+//同じ名前のセレクタに同じ処理をしたい時ALLで取得して、forEachで繰り返し処理の中身を書く
+
+// const modalImg = document.querySelectorAll('.modal-img');
+// modalImg.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         item.classList.toggle('active');
+//     });
+// });
