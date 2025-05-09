331968621e5ab0bd58583a4db0d53c35e0687260
meccin5678 <meccin56@gmail.com>
2025-05-09 13:58:47 +0900

    スクロールトリガー追加

--- sampleG/script.js
+++ sampleG/script.js
@@ -1,10 +1,22 @@
-// -----------------------------
-// ✨ GSAP
-// -----------------------------
 document.addEventListener("DOMContentLoaded", () => {
-  // GSAPで回転アニメーションを適用
   gsap.registerPlugin(ScrollTrigger);
 
+  // ✨ GSAP 実感レポート
+  gsap.utils.toArray(".secFlex").forEach((section) => {
+    gsap.from(section, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: section,
+        start: "top 80%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
+
+  // ✨ GSAP 原材料
   gsap.fromTo(
     "#fade",
     {
