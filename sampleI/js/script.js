f906a5423c3a17a94b84e05de7d5a7561c614aa6
meccin5678 <meccin56@gmail.com>
2025-06-05 13:23:58 +0900

    スクロールOKにした

--- sampleI/js/script.js
+++ sampleI/js/script.js
@@ -6,7 +6,7 @@
 document.querySelector('.trial .try-try-btn').addEventListener('click', function () {
   document.querySelector('.trial .modal-try-content').classList.add('is-active');
   document.querySelector('.trial .overlay').classList.add('is-active');
-  document.body.style.overflow = 'hidden'; // ← スクロール禁止
+  // document.body.style.overflow = 'hidden';
 });
 
 // モーダルを閉じる
@@ -14,6 +14,6 @@ document.querySelectorAll('.trial .modal-try-content .tryClose').forEach(btn => 
   btn.addEventListener('click', function () {
     document.querySelector('.trial .modal-try-content').classList.remove('is-active');
     document.querySelector('.trial .overlay').classList.remove('is-active');
-    document.body.style.overflow = ''; // ← スクロール解除
+    // document.body.style.overflow = ''; 
   });
 });
