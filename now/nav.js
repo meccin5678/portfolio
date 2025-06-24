fd0bccdfa3b15b8fb50d9c9b1562e5aa349da0d4
meccin5678 <meccin56@gmail.com>
2025-06-24 21:38:07 +0900

    css

--- now/nav.js
+++ now/nav.js
@@ -1,4 +1,4 @@
-// ページ内リンクのスムーススクロール
+// // ページ内リンクのスムーズスクロール　対応1
 // document.addEventListener('DOMContentLoaded', () => {
 //   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //     anchor.addEventListener('click', e => {
@@ -14,19 +14,32 @@
 //   });
 // });
 
+// // ページ内リンクのスムーズスクロール　対応2
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', e => {
+//       const targetId = anchor.getAttribute('href');
+//       console.log('targetId:', targetId);
+//       const target = document.querySelector(targetId);
+//       console.log('target:', target);
+//       if (targetId.length > 1 && target) {
+//         e.preventDefault();
+//         const y = target.getBoundingClientRect().top + window.pageYOffset - 80;
+//         // window.scrollToは、ページ全体を指定した位置までスクロールするメソッドです。
+//         // top: y でスクロール先のY座標（ページ上端からのピクセル数）を指定します。
+//         // behavior: 'smooth' でスクロールをなめらかにアニメーションします。
+//         window.scrollTo({ top: y, behavior: 'smooth' });
+//       }
+//     });
+//   });
+// });
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', e => {
-      const targetId = anchor.getAttribute('href');
-      console.log('targetId:', targetId);
-      const target = document.querySelector(targetId);
-      console.log('target:', target);
-      if (targetId.length > 1 && target) {
-        e.preventDefault();
-        const y = target.getBoundingClientRect().top + window.pageYOffset - 80;
-        window.scrollTo({ top: y, behavior: 'smooth' });
-      }
-    });
-  });
-});
+
+setTimeout(() => {
+  const target = document.querySelector(location.hash);
+  if (target) {
+    const offset = 80;
+    const y = target.getBoundingClientRect().top + window.scrollY - offset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+}, 500);
